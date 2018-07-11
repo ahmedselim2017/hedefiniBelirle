594643ec11118e4114c549042f1f2731c61e0bce
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-07-11 21:59:42 +0300

    Ders-121

--- HedefiniBelirle/Controller/HedefVC.swift
+++ HedefiniBelirle/Controller/HedefVC.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HedefVC: UIViewController {
 
+    @IBOutlet weak var tabloGoruntuleyici: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnHedefEkleBasildi(_ sender: Any) {
+        print("hedef ekle");
+    }
+    
 }
 
