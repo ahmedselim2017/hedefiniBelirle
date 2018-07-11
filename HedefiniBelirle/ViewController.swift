3c7eddc819e14c6e9eb9dee198129e568df14f5c
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-07-11 20:13:26 +0300

    Proje Dosyaları Olusturuldu

---
+++ HedefiniBelirle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HedefiniBelirle
+//
+//  Created by Ahmed Selim Üzüm on 11.07.2018.
+//  Copyright © 2018 Ahmed Selim Üzüm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
